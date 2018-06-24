419e0f31bcdc16d41c9e7237b3888a8c2213ef31
mushipeas <muj990@gmail.com>
2018-06-24 21:57:36 +0500

    Remove Generic Header
    Remove the header that was present from create-react-app.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import MainWindow from './components/MainWindow';
@@ -142,10 +141,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <Header/>
         <div className="body">
           <MainWindow
