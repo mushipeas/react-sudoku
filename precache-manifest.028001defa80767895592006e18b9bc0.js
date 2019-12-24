7966fb1cc427a31bebecdb3a4b2fb980fd870948
mushipeas <8151412+mushipeas@users.noreply.github.com>
2019-12-24 07:27:54 +0000

    Updates

---
+++ precache-manifest.028001defa80767895592006e18b9bc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8966376f5e08be06193c110c1851c13",
+    "url": "/react-sudoku/index.html"
+  },
+  {
+    "revision": "9777b20e3e680e2a4702",
+    "url": "/react-sudoku/static/css/main.e67d3a95.chunk.css"
+  },
+  {
+    "revision": "44a87ee6ba5819f57b8b",
+    "url": "/react-sudoku/static/js/2.5709aedd.chunk.js"
+  },
+  {
+    "revision": "d8acfc518a74bbee4a2a25e3f2733ff1",
+    "url": "/react-sudoku/static/js/2.5709aedd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9777b20e3e680e2a4702",
+    "url": "/react-sudoku/static/js/main.2110b8fb.chunk.js"
+  },
+  {
+    "revision": "bd59fb0ba4291a4dbf4c",
+    "url": "/react-sudoku/static/js/runtime-main.02b25799.js"
+  }
+]);
